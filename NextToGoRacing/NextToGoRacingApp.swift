5dee3d63537f3e833ff6ba19f14747cf995f6df7
Zhiying Fan <fanzhiying1992@gmail.com>
2024-12-04 13:54:30 +1100

    feat: create the initial project

---
+++ NextToGoRacing/NextToGoRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoRacingApp.swift
+//  NextToGoRacing
+//
+//  Created by Zhiying Fan on 4/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
