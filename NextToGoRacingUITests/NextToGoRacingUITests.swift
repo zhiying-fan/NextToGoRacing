b2d8014f1f8f8621d9114c3e82084947de8fb823
Zhiying Fan <fanzhiying1992@gmail.com>
2024-12-06 17:58:59 +1100

    test: add test case for filter functionality

--- NextToGoRacingUITests/NextToGoRacingUITests.swift
+++ NextToGoRacingUITests/NextToGoRacingUITests.swift
@@ -22,14 +22,34 @@ final class NextToGoRacingUITests: XCTestCase {
         app.terminate()
     }
 
-    func testRacingViewWithValidRaces() {
+    func testRacingView_whenUnselectedHorse_shouldRemoveAllHorseRaces() {
+        // Check race info on racing list
         let listView = app.collectionViews
         let meetingName = listView.staticTexts["Parx Racing"]
         let raceNumber = listView.staticTexts["Race 6"]
         let state = listView.staticTexts["Ongoing"]
 
-        XCTAssert(meetingName.waitForExistence(timeout: 1), "Failed to find meeting name in racing screen")
-        XCTAssert(raceNumber.waitForExistence(timeout: 1), "Failed to find race number in racing screen")
-        XCTAssert(state.waitForExistence(timeout: 1), "Failed to find state in racing screen")
+        XCTAssert(meetingName.waitForExistence(timeout: 0.1), "Failed to find meeting name on racing screen")
+        XCTAssert(raceNumber.waitForExistence(timeout: 0.1), "Failed to find race number on racing screen")
+        XCTAssert(state.waitForExistence(timeout: 0.1), "Failed to find state on racing screen")
+
+        // Tap filter button
+        let filterMenu = app.navigationBars.staticTexts["Filter"]
+
+        XCTAssert(filterMenu.waitForExistence(timeout: 0.1), "Failed to find filter menu on racing screen")
+
+        filterMenu.tap()
+
+        let filterView = app.collectionViews
+        let horseButton = filterView.buttons["Horse"]
+
+        XCTAssert(horseButton.waitForExistence(timeout: 0.1), "Failed to find horse button on filter screen")
+
+        // Unselect horse category
+        horseButton.tap()
+        app.tap()
+
+        // Check horse races are removed
+        XCTAssertFalse(meetingName.waitForExistence(timeout: 0.1), "Failed to filter out horse races")
     }
 }
